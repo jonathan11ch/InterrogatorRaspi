8b32d58748105a18ae7d086b75fb3a408aede2d1
jonathan11ch <jonathan11ch@hotmail.com>
2018-09-12 17:42:32 -0300

    added parallel reading for 16 bits

--- hal/gpio_reader.py
+++ hal/gpio_reader.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import threading 
 #pgio ports to be used:
-#gpio 23,24,4,17,27,22,10,9,11,5
+#gpio 23,24,4,17,27,22,10,9,11,14
 #pin  3,5,7,11,13,15,19,21,23,29
 
 class GpioReader(object):
@@ -20,6 +20,14 @@ class GpioReader(object):
 		self.Bit6 = 22
 		self.Bit7 = 10
 		self.Bit8 = 9
+		self.Bit9 = 25
+		self.Bit10 = 8
+		self.Bit11 = 7
+		self.Bit12 = 12
+		self.Bit13 = 16
+		self.Bit14 = 20
+		self.Bit15 = 21
+		self.Bit16 = 26  
 		self.Rx   = 11
 		self.Tx   = 14
 		#setup input ports
@@ -31,6 +39,14 @@ class GpioReader(object):
 		GPIO.setup(self.Bit6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		GPIO.setup(self.Bit7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		GPIO.setup(self.Bit8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit9, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.Bit16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		GPIO.setup(self.Rx, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 		#setup output port
 		GPIO.setup(self.Tx, GPIO.OUT, initial = GPIO.LOW)
@@ -38,7 +54,7 @@ class GpioReader(object):
 		#GPIO.add_event_callback(self.Rx, self.onDataCallback)
 		GPIO.add_event_detect(self.Rx, GPIO.RISING, callback=self.onDataCallback, bouncetime=3)
 		#create data buffer
-		self.buffer = np.array([0]*8)		
+		self.buffer = np.array([0]*16)		
 
 	#read register function	
 	def read_register(self):
@@ -50,6 +66,14 @@ class GpioReader(object):
 		self.buffer[5] = GPIO.input(self.Bit6)
 		self.buffer[6] = GPIO.input(self.Bit7)
 		self.buffer[7] = GPIO.input(self.Bit8)
+		self.buffer[8] = GPIO.input(self.Bit9)
+		self.buffer[9] = GPIO.input(self.Bit10)
+		self.buffer[10] = GPIO.input(self.Bit11)
+		self.buffer[11] = GPIO.input(self.Bit12)
+		self.buffer[12] = GPIO.input(self.Bit13)
+		self.buffer[13] = GPIO.input(self.Bit14)
+		self.buffer[14] = GPIO.input(self.Bit15)
+		self.buffer[15] = GPIO.input(self.Bit16)
 
 	def convert_to_integer(self):
 		s = ""
